98687d4998158d895780e738fcdc148f1fe15bab
yurii-kovalchuk <sogdrums@gmail.com>
2023-10-09 18:12:52 +0300

    init project

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.description}>The Cat App</div>
+    </main>
+  );
+}
