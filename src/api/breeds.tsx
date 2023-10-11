4bde1edc7010fd54427b8a468b7cdba6c8161d53
yurii-kovalchuk <sogdrums@gmail.com>
2023-10-11 18:11:57 +0300

    set up breeds, votes, favourites api functions

--- src/api/breeds.tsx
+++ src/api/breeds.tsx
@@ -1,8 +1,20 @@
+import { QueryParams } from "@/types/common";
 import catApiClient from "./api";
 
-export const getBreeds = async () => {
+export const getBreeds = async (queryParams: QueryParams = {}) => {
   try {
-    const { data } = await catApiClient.get("/breeds");
+    const { data } = await catApiClient.get("/breeds", {
+      params: queryParams,
+    });
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const getBreed = async (breedId: string) => {
+  try {
+    const { data } = await catApiClient.get(`/favourites/${breedId}`);
     return data;
   } catch (err) {
     console.log(err);
