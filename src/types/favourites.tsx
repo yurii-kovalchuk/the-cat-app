996690c981b2cfeba90e9803b52654df2ce6160c
yurii-kovalchuk <106819147+yurii-kovalchuk@users.noreply.github.com>
2023-10-20 15:17:20 +0300

    Revert "Revert "update votes and favourites types""
    This reverts commit b2539a5213991ff9ae99e10c1b2f93188c2e9249.

--- src/types/favourites.tsx
+++ src/types/favourites.tsx
@@ -4,18 +4,16 @@ export type Favourite = {
   id: string;
   user_id: string;
   image_id: string;
-  sub_id: string | null;
+  sub_id?: string | null;
   created_at: string;
   image: Pick<Image, "id" | "url">;
   message: string;
 };
 
-export type FavouriteInstance = Pick<Favourite, "image_id"> & {
-  sub_id?: string;
-};
+export type FavouriteInstance = Pick<Favourite, "image_id" | "sub_id">;
 
-export type GetFavourite = Omit<Favourite, "message">;
+export type GetFavouriteResponse = Omit<Favourite, "message">;
 
-export type CreateFavourite = Pick<Favourite, "message" | "id">;
+export type CreateFavouriteResponse = Pick<Favourite, "message" | "id">;
 
-export type DeleteFavourite = Pick<Favourite, "message">;
+export type DeleteFavouriteResponse = Pick<Favourite, "message">;
