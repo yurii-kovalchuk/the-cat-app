ffedaa58679d33df9a390318299a1d5761dc78ea
yurii-kovalchuk <106819147+yurii-kovalchuk@users.noreply.github.com>
2023-10-20 14:57:44 +0300

    Revert "set up create and read operation for upload own images"
    This reverts commit 79ffa205ccc5b19449abe1cf2406be5fd7d70bf8.

--- src/api/images.tsx
+++ src/api/images.tsx
@@ -1,37 +1,10 @@
 import catApiClient from "./api";
 import { QueryParams } from "@/types/common";
 
-type Image = {
-  file: File;
-  sub_id?: string;
-};
-
 export const getImages = async (queryParams: QueryParams = {}) => {
   try {
     const { data } = await catApiClient.get("/images/search", {
       params: queryParams,
-    });
-    return data;
-  } catch (err) {
-    console.log(err);
-  }
-};
-
-export const getCreatedImages = async (queryParams: QueryParams = {}) => {
-  try {
-    const { data } = await catApiClient.get("/images", {
-      params: queryParams,
-    });
-    return data;
-  } catch (err) {
-    console.log(err);
-  }
-};
-
-export const createImage = async (image: Image) => {
-  try {
-    const { data } = await catApiClient.post("/images/upload", image, {
-      headers: { "Content-Type": "multipart/form-data" },
     });
     return data;
   } catch (err) {
