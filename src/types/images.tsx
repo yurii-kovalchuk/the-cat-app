a6f07e3ea211eff510822c594544fa180dd2e33f
yurii-kovalchuk <106819147+yurii-kovalchuk@users.noreply.github.com>
2023-10-20 15:17:20 +0300

    Revert "Revert "update image types""
    This reverts commit ba51dcc01299a5ae0f7c04680826c4d15af1f89b.

--- src/types/images.tsx
+++ src/types/images.tsx
@@ -6,31 +6,33 @@ export type Image = {
   url: string;
   width: string;
   height: string;
-  mime_type: string;
-  entities?: [];
-  animals?: [];
-  categories?: Categorie[];
-  breeds: Breed[];
+  categories?: Categorie[] | [];
+  breeds: Breed[] | [];
   pending: string;
   approved: string;
+  original_filename: string;
+  sub_id?: string | null;
+  created_at: string;
+  breed_ids?: string;
 };
 
-export type GetImage = Pick<
+export type GetImageResponse = Pick<
   Image,
   "breeds" | "categories" | "id" | "url" | "width" | "height"
 >;
 
-export type GetOwnImage = Omit<Image, "pending" | "approved" | "breeds"> & {
-  breeds: {
-    id: string;
-    name: string;
-    wikipedia_url: string;
-  }[];
+export type GetOwnImageResponse = Omit<
+  Image,
+  "categories" | "pending" | "approved" | "breeds"
+> & {
+  breeds: Pick<Breed, "id" | "name" | "wikipedia_url">[] | [];
 };
 
-export type CreateImageResp = {
-  id: string;
-  url: string;
-  pending: string;
-  approved: string;
+export type CreateImageResponse = Omit<
+  Image,
+  "categories" | "breeds" | "created_at"
+>;
+
+export type ImageInstance = Pick<Image, "sub_id"> & {
+  file: Blob;
 };
